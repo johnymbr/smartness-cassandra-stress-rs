8f9d296ab1ff3394b72cc271958ee1809f6a6955
Johny marques <johnymbr@gmail.com>
2025-11-10 21:37:05 -0300

    fix insert script.

--- pyscripts/smartness_generate_inserts.py
+++ pyscripts/smartness_generate_inserts.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
         reader = csv.reader(csvfile)
         headers = next(reader)
 
-    columns = ", ".join([f"\\\"{col}\\\"" for col in headers[:100]])
-    values = ", ".join([f"?" for col in headers[:100]])
+    columns = ", ".join([f"\\\"{col}\\\"" for col in headers[:300]])
+    values = ", ".join([f"?" for col in headers[:300]])
 
     print(f"INSERT INTO {table_name} (\\\"id\\\", {columns}) VALUES (?, {values});")
